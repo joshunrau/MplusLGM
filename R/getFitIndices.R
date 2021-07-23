38e0a91a0afbb85c0c09785847786b0c3e662abe
joshunrau <joshuatunrau@protonmail.com>
2021-07-22 20:54:34 -0400

    add CAIC

--- R/getFitIndices.R
+++ R/getFitIndices.R
@@ -1,20 +1,17 @@
 #' @title getFitIndices
 #' @description Returns a selection of fit indices for a list of MplusObjects
 #' @param list_models A list containing MplusObjects
-#' @param type_stats A character vector containing the statistics to return
 #' @return A data frame
 #' @export
 #' @import MplusAutomation
 #' @import stringr
-getFitIndices <- function(
-  list_models, 
-  type_stats = c("Title", 'LL', "BIC", "Entropy", "T11_LMR_Value", "T11_LMR_PValue")
-  ) {
+getFitIndices <- function(list_models) {
   
   # Iterate through all models in list to extract results from path
   model_res_list <- list()
   count <- 1
   
+  n_sample <- NULL  # the sample size for all models must be the same for CAIC
   for (model in list_models) {
     
     # Get the filepath of this model's output
@@ -26,12 +23,26 @@ getFitIndices <- function(
     model_res <- MplusAutomation::readModels(path_out, what = 'summaries')
     model_res_list[[count]] <- model_res
     count <- count + 1
+    
+    # check sample size for all models must be the same for CAIC
+    if (is.null(n_sample)) {
+      n_sample <- model_res[["summaries"]][["Observations"]]
+    } else {
+      stopifnot(n_sample == model_res[["summaries"]][["Observations"]])
+    }
   }
   
   # Create table of these models and return
   models_sum <- MplusAutomation::SummaryTable(
     model_res_list, 
-    keepCols = type_stats)
+    keepCols = c("Title", 'LL', 'Parameters', "BIC", "Entropy", 
+                 "T11_LMR_Value", "T11_LMR_PValue"))
+  
+  models_sum$CAIC <- -2 * models_sum$LL + models_sum$Parameters * (log(n_sample) + 1)
+  
+  models_sum <- subset(models_sum, select = c("Title", 'LL', "BIC", "CAIC", 
+                                              "Entropy", "T11_LMR_Value", 
+                                              "T11_LMR_PValue"))
   
   return(models_sum)
   
