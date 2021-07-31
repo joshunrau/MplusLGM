cc6f3ac561519fe72d9915033d585114711a23f0
joshunrau <78170036+joshunrau@users.noreply.github.com>
2021-07-30 20:30:57 -0400

    update to run ANOVA w/ vtable

--- R/getSummaryTable.R
+++ R/getSummaryTable.R
@@ -1,173 +1,77 @@
 #'@title getSummaryTable
 #'@param df A data frame containing all descriptive variables and the grouping variable
-#'@param group_var A factor defining the grouping variable
 #'@param list_vars A vector containing the variables to summarize
+#'@param var_labels A vector of labels for all in list_var
+#'@param group The variable name for grouping
+#'@param group.test Boolean, specify whether vtable will run ANOVA/ChiSq
 #'@return A data frame
 #'@export
 #'@import tidyverse
-getSummaryTable <- function(
-  df, 
-  list_vars,
-  group_var = NULL
-  ) {
-  
-  # Input validation
-  stopifnot(
-    is.data.frame(df),
-    is.factor(df[,group_var]) || is.null(group_var), 
-    is.vector(list_vars),
-    list_vars %in% colnames(df)
+#'@import vtable
+
+getSummaryTable <- function(df, list_vars, var_labels, group = NA, group.test = FALSE) {
+
+  tab <-  vtable::sumtable(
+    data = df,
+    vars = list_vars,
+    labels = var_labels,
+    summ = c('notNA(x)','mean(x)','sd(x)'),
+    summ.names = c('N','MP','SD'),
+    group = group,
+    group.test = group.test,
+    out = "return",
+    digits = 2
   )
   
-  # If no group variable, we will assign it to be 'overall'
-  if (is.null(group_var)) {
-    sample_label <- paste0('Overall (N=', c(nrow(df)), ')')
-    df$Sample <- factor(rep(sample_label, nrow(df)))
-    group_var <- 'Sample'
-  }
   
-  # Create list for all continuous and categorical variables
-  all_categorical <- c()
-  all_continuous <- c()
-  
-  # Sort the variables and make sure all are either factors or numeric
-  for (var_name in list_vars) {
+  # If not by group
+  if (is.na(group)) {
     
-    if (is.factor(df[,var_name])) {
-      all_categorical <- c(all_categorical, var_name)
-    } 
+    tab$Overall <- ifelse(
+      tab$SD != '',
+      tab$Overall <- paste0('M = ', tab$MP, ' ± ', tab$SD),
+      ifelse(
+        tab$MP != '',
+        tab$Overall <- paste0('N = ', tab$N, ' (', tab$MP, ')'),
+        tab$MP <- ''
+      )
+    )
     
-    else if (is.numeric(df[,var_name])){
-      all_continuous <- c(all_continuous, var_name)
+    return(subset(tab, select = c('Variable', 'Overall')))
+    
+  } else {
+    new_list <- list(Variable = c(tab[2:nrow(tab), 1]))
+    
+    for (c in 2:length(tab)) { # skip if the name of grouping variable
+      
+      if (tab[1 , c] != '') { # if the name of level of group variable
+        
+        group_res <- c()
+        for (r in 2:nrow(tab)) { # iterate through rows which correspond to N
+          
+          # get values from mean/percent and SD
+          n_tmp <- tab[r , c]
+          mp_tmp <- tab[r , c+1]
+          sd_tmp <- tab[r , c+2]
+          
+          if (mp_tmp == '' && sd_tmp == '') {
+            group_res <- c(group_res, '')
+          } 
+          
+          else if (sd_tmp == '') {
+            group_res <- c(group_res, paste0('N = ', n_tmp, ' (', mp_tmp, ')'))
+          }
+          
+          else {
+            group_res <- c(group_res,  paste0('M = ', mp_tmp, ' ± ', sd_tmp))
+          }
+          
+        }
+        
+        new_list[[tab[1 , c]]] <- unlist(group_res)
+        
+      }
     }
-    
-    else {
-      stop('all variables must be either factors or numeric')
-    }
-
+    return(data.frame(new_list))
   }
-  
-  # Get the means for all continuous variables
-  var_means <- df %>%
-    dplyr::group_by(df[,group_var]) %>% 
-    dplyr::summarise_at(vars(all_of(all_continuous)), mean, na.rm = TRUE)
-  
-  # Get the standard deviations for all continuous variables
-  var_sd <- df %>%
-    dplyr::group_by(df[,group_var]) %>% 
-    dplyr::summarise_at(vars(all_of(all_continuous)), sd, na.rm = TRUE)
-  
-  # Get grouping variable levels
-  group_var_labs <- as.character(var_means[[1]])
-  stopifnot(group_var_labs == as.character(var_sd[[1]]))
-  
-  # Transpose data frames
-  var_means <- round(as.data.frame(t(var_means[,-1])), digits = 2)
-  var_sd <- round(as.data.frame(t(var_sd[,-1])), digits = 2)
-  
-  # Combine mean and sd data frames 
-  cont_vars_summary <- list()
-  for (i in 1:length(group_var_labs)) {
-    name_var <- group_var_labs[[i]]
-    cont_vars_summary[[name_var]] <- paste0(var_means[,i], ' (', var_sd[,i], ')')
-  }
-  
-  cont_vars_summary <- data.frame(cont_vars_summary)
-
-  # Rename variables in dataframe
-  rownames(cont_vars_summary) <- all_continuous
-  
-  # Reapply correct column names
-  colnames(cont_vars_summary) <- group_var_labs
-  
-  # GET FOR CATEGORICAL VARS
-  
-  # Get nested list of stats
-  
-  cat_vars_summaries <- list()
-  for (i in match(all_categorical, colnames(df))) {
-    
-    # Get name of variable
-    name_var <- colnames(df[i])
-    
-    # Get the counts for each level of the factor
-    for (j in group_var_labs) {
-      
-      # Subset data frame to contain only the level being tested
-      df_tmp <- df %>%
-        dplyr::filter(.data[[group_var]] == toString(j))
-      
-      cat_vars_summaries[[name_var]][[toString(j)]] <- table(df_tmp[[name_var]])
-      
-    }
-    
-  }
-  
-  # Reorganize nested lists to dataframe
-  
-  suppressWarnings(rm(cat_vars_summary))
-
-  for (v in labels(cat_vars_summaries)) {
-    
-    if (!exists('cat_vars_summary')) {
-      
-      cat_vars_summary <- .listTablesToDF(cat_vars_summaries[[v]], group_var, v, df)
-      
-    } else {
-      
-      cat_vars_summary <- rbind(cat_vars_summary,
-                                .listTablesToDF(cat_vars_summaries[[v]], group_var, v, df))
-      
-    }
-    
-  }
-  
-  # Make sure all columns are the same. Then merge and return.
-  
-  final_summary <- rbind(cont_vars_summary, cat_vars_summary)
-  
-  return(final_summary)
-  
-}
-
-
-.listTablesToDF <- function(list_tables, group_var, varName, df) {
-  
-  for (i in 1:length(list_tables)) {
-    
-    # get grouping var level 
-    group_var_tmp <- labels(list_tables[i])
-    
-    df_sum <- df %>% filter(.data[[group_var]] == group_var_tmp)
-    
-    sum_var <- sum(!is.na(df_sum[[varName]]))
-    
-    # get the table
-    table_tmp <- data.frame(list_tables[[i]])
-    table_tmp[['Perc']] <- round((table_tmp[['Freq']] / sum_var) * 100, digits = 2)
-    
-    # reorganize the table
-    row.names(table_tmp) <- table_tmp[[1]]
-    table_tmp[[group_var_tmp]] <- paste0(table_tmp[['Freq']], ' (', 
-                                         table_tmp[['Perc']], '%)')
-    table_tmp <- subset(table_tmp, select = group_var_tmp)
-    
-    if (i == 1) {
-    
-      new_tables <- table_tmp
-      
-    } else {
-      new_tables <- cbind(new_tables, table_tmp)
-    }
-    
-  }
-  
-  # Add the entry row
-  entry_row <- data.frame(t(replicate(ncol(new_tables), ' ')))
-  colnames(entry_row) <- labels(list_tables)
-  rownames(entry_row) <- varName
-  new_tables <- rbind(entry_row, new_tables)
-  
-  return(new_tables)
-         
 }
